b863b0aadf8987774169d790098de58ea52ecf2f
anilappana999 <anilappana999@gmail.com>
2021-08-16 07:47:51 +0530

    Grading students hackerrank-solution

---
+++ hackerrank/src/problemsolving/GradingStudents.java
@@ -0,0 +1,33 @@
+package problemsolving;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GradingStudents {
+//code starts here
+	public static List<Integer> gradingStudents(List<Integer> grades) {
+	
+	    List<Integer> grades1 = new ArrayList<>();
+	    for(Integer i:grades ){
+	      Integer num =i;
+	      Integer num1 = (Integer)Math.round(num/5)*5;
+	      num1+=5;
+	      if(i<38||num1-num>=3){
+	          grades1.add(i);
+	      }
+	      else{
+	          grades1.add(num1);
+	      }
+	        
+	    }
+	return grades1;
+	    }
+	//code ends here
+	public static void main(String[] args) {
+		
+
+		
+
+	}
+
+}
